c21468a720427e96619a13997714965703030109
hrmguilherme2 <39834493+hrmguilherme2@users.noreply.github.com>
2019-01-29 17:31:40 -0200

    Update Semantix.scala

--- scala/Semantix.scala
+++ scala/Semantix.scala
@@ -42,7 +42,7 @@ object  Semantix {
   
   // Contagem de hosts
   def hostsunicos(rdd: RDD[String],mes:String) = {
-    var dt = rdd.flatMap(line => line).distinct().count()
+    var dt = rdd.flatMap(line => line.split('-')(0).split(' ')).distinct().count()
     println("Mostrado hosts distintos ", mes + " " +  dt)
 
   }
@@ -54,7 +54,7 @@ object  Semantix {
 
   // Contagem de 5 404
   def urls5top(rdd: RDD[String],mes:String) = {
-    var urls = rdd.map( line => line.split('"')(1).split(' ')(1))
+    var urls = rdd.map( line => line.split(' ')(0))
     var counts = urls.map( endpoint => (endpoint, 1)).reduceByKey((a, b) => a + b)
     var top = counts.sortBy( pair => -pair._2).take(5)
     print("\nTop 5 404 erros\n")
